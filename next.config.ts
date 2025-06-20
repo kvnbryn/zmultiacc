0b546b820c8cf0941e42f2634036a9149209a0d7
kvnbryn <151827022+kvnbryn@users.noreply.github.com>
2025-06-20 17:26:43 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Bungkus konfigurasi Server Actions di dalam blok 'experimental'
   experimental: {
     serverActions: {
-      // Naikkan batas ukuran body request.
       bodySizeLimit: '10mb',
     },
-  }
-};
-  // TAMBAHKAN BLOK INI
-  // Ini akan memberitahu Vercel untuk mengabaikan error TypeScript saat build.
+  },
+
   typescript: {
-    // PERINGATAN: Opsi ini secara sengaja mengizinkan build produksi
-    // untuk berhasil meskipun proyek Anda memiliki error tipe data.
     ignoreBuildErrors: true,
   },
 };
