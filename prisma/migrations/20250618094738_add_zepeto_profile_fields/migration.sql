23989a38ae3f5d0919ac7b23efe3c62c283690e7
kvnbryn <151827022+kvnbryn@users.noreply.github.com>
2025-06-20 18:49:41 +0800

    Delete prisma/migrations directory

--- prisma/migrations/20250618094738_add_zepeto_profile_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "ZepetoAccount" ADD COLUMN "displayName" TEXT;
-ALTER TABLE "ZepetoAccount" ADD COLUMN "profilePic" TEXT;
-ALTER TABLE "ZepetoAccount" ADD COLUMN "username" TEXT;
